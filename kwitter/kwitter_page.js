6700a7fe6c52a0e68e9177ee1de76f31466545d4
LetsChat-Framework <letschatwebappmail@gmail.com>
2024-09-05 19:47:50 +0530

    Removed backend.
    Removed the Kwitter backend.
    
    Signed-off-by: LetsChat-Framework <letschatwebappmail@gmail.com>

--- kwitter/kwitter_page.js
+++ kwitter/kwitter_page.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyBQyjrjTsIQsGMGcgu-cr1HjszcHi5ZWMk",
-  authDomain: "testkwitter.firebaseapp.com",
-  databaseURL: "https://testkwitter.firebaseio.com",
-  projectId: "testkwitter",
-  storageBucket: "testkwitter.appspot.com",
-  messagingSenderId: "624653701634",
-  appId: "1:624653701634:web:2cb9a8bd873f17d92d8d1b"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
